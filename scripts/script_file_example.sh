226ec41e0b49084e38d9b3277a0ba3fac134c506
irles <irles@lal.in2p3.fr>
2017-02-02 12:25:54 +0100

    update scripts

--- scripts/script_file_example.sh
+++ scripts/script_file_example.sh
@@ -4,27 +4,40 @@
 # Example script to run monitoring and analysis over one file
 
 FOLDER_INPUT="/home/irles/WorkArea/TestBench/2016/rawdata/daq_tests/20161215_123714/"
-FOLDER_OUTPUT="/home/irles/WorkArea/TestBench/output_tests/"
-FILE="scurves_trig211_by_dif0.raw"
+FOLDER_OUTPUT="/home/irles/WorkArea/TestBench/2017/results/20161215_123714/by8_masknoisy/"
 
-echo "Input file: " $FOLDER_INPUT
-echo "Output Folder:: " $FOLDER_OUTPUT
+for i in 0 1 2
+do
+    if [ $i == 0 ]; then 
+	FILE="scurves_trig178_by_dif0.raw"
+    fi
+    if [ $i == 1 ]; then 
+	FILE="scurves_trig187_by_dif0.raw"
+    fi
+    if [ $i == 2 ]; then 
+	FILE="scurves_trig211_by_dif0.raw"
+    fi
 
-if [ -d "$FOLDER_INPUT" ];then
-    FULLPATH=$FOLDER_INPUT$FILE".root"
-    echo $FULLPATH
-    if [ -f "$FULLPATH" ];then  
-	if [ ! -d "$FOLDER_OUTPUT" ];then
-	    echo "Output directory " $FOLDER_OUTPUT "does not exist, I am creating it"
-	    mkdir $FOLDER_OUTPUT
+    echo "Input file: " $FOLDER_INPUT
+    echo "Output Folder:: " $FOLDER_OUTPUT
+    
+    if [ -d "$FOLDER_INPUT" ];then
+	FULLPATH=$FOLDER_INPUT$FILE".root"
+	echo $FULLPATH
+	if [ -f "$FULLPATH" ];then  
+	    if [ ! -d "$FOLDER_OUTPUT" ];then
+		echo "Output directory " $FOLDER_OUTPUT "does not exist, I am creating it"
+		mkdir $FOLDER_OUTPUT
+	    fi
+	    FILE_OUTPUT=$FOLDER_OUTPUT$FILE
+	    xterm -geometry 93x31+100+10 -e "./../bin/tpecalana  $FULLPATH $FILE_OUTPUT MonitorChannel 16" &
+	    xterm -geometry 93x31+700+10 -e  "./../bin/tpecalana  $FULLPATH $FILE_OUTPUT MonitorChip 16" &
+	    #	./../bin/tpecalana  $FULLPATH $FILE_OUTPUT Pedestal 16
+	    #	./../bin/tpecalana  $FULLPATH $FILE_OUTPUT MIP 16
 	fi
-	FILE_OUTPUT=$FOLDER_OUTPUT$FILE
-	xterm -geometry 93x31+100+10 -e "./../bin/tpecalana  $FULLPATH $FILE_OUTPUT MonitorChannel 16" &
-	xterm -geometry 93x31+700+10 -e  "./../bin/tpecalana  $FULLPATH $FILE_OUTPUT MonitorChip 16" &
-#	./../bin/tpecalana  $FULLPATH $FILE_OUTPUT Pedestal 16
-#	./../bin/tpecalana  $FULLPATH $FILE_OUTPUT MIP 16
-fi
-else
-    echo "Input file " $FOLDER_INPUT "does not exist, I am stopping"
-fi
+    else
+	echo "Input file " $FOLDER_INPUT "does not exist, I am stopping"
+    fi
+    sleep 20
+done
     
--- scripts/script_scan_example.sh
+++ scripts/script_scan_example.sh
@@ -1,16 +1,11 @@
 #!/bin/bash                                                                                                                                                                    
-FOLDER_INPUT="/airqui/LAL/testbench_nov2016/rawdata/Scurves_FEV8_COB_HG_8channels_0"
-FOLDER_OUTPUT="/airqui/LAL/testbench_nov2016/rawdata/output_tests_HG_8C_0/"
-mkdir "/home/"$FOLDER_OUTPUT
+FOLDER_INPUT="/home/irles/WorkArea/TestBench/2016/rawdata/daq_tests/20161215_123714/scurves_by8_masknoisy/"
+FOLDER_OUTPUT="/home/irles/WorkArea/TestBench/2017/results/20161215_123714/by8_masknoisy/"
+#mkdir $FOLDER_OUTPUT
+#./../bin/tpecalana  $FOLDER_INPUT $FOLDER_OUTPUT PlaneEventsScan 16 8
 
-#./../bin/tpecalana  $FOLDER_INPUT $FOLDER_OUTPUT PlaneEventsScan 1 0
-#./../tpecalana  $FOLDER_INPUT $FOLDER_OUTPUT scurves 1 0 0 
-#./../tpecalana  $FOLDER_INPUT $FOLDER_OUTPUT scurves 1 0 1
+xterm -e "time ./../bin/tpecalana  $FOLDER_INPUT $FOLDER_OUTPUT PlaneEventsScan 16 8" &
+xterm -e "time ./../bin/tpecalana  $FOLDER_INPUT $FOLDER_OUTPUT scurves 16 8 0" &
+xterm -e "time ./../bin/tpecalana  $FOLDER_INPUT $FOLDER_OUTPUT scurves 16 8 1" &
+xterm -e "time ./../bin/tpecalana  $FOLDER_INPUT $FOLDER_OUTPUT scurves 16 8 2" 
 
-FOLDER_INPUT="/airqui/LAL/testbench_nov2016/rawdata/Scurves_FEV8_COB_HG_8channels_1"
-FOLDER_OUTPUT="/airqui/LAL/testbench_nov2016/rawdata/output_tests_HG_8C_1/"
-mkdir "/home/"$FOLDER_OUTPUT
-
-xterm -e "./../tpecalana  $FOLDER_INPUT $FOLDER_OUTPUT PlaneEventsScan 1 0" &
-xterm -e "./../tpecalana  $FOLDER_INPUT $FOLDER_OUTPUT scurves 1 0 0" &
-xterm -e "./../tpecalana  $FOLDER_INPUT $FOLDER_OUTPUT scurves 1 0 1" &
