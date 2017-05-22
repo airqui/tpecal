af010cafd819d2c7d567754fc5e4bc10441be86f
irles <irles@lal.in2p3.fr>
2017-05-22 11:34:28 +0200

    remove root macros

--- root_macros/RunMerge4Scurves.C
+++
@@ -1,7 +0,0 @@
-void RunMerge4Scurves(string dirname, int start, int stop, int step)
-{
-	gSystem->Load("/opt/calicoes/standard/Merge4Scurves_C");
-        M=Merge4Scurves();
-        M.DoMerge(dirname,false,start,stop,step);
-	gSystem->Exit(0);
-}
